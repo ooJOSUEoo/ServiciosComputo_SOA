106319f044c16ac4884e6e0688b03b7c5079a647
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-20 14:39:12 -0500

    Redireccion de login, de home a index

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const INDEX = '/servicio';
 
     /**
      * The controller namespace for the application.
