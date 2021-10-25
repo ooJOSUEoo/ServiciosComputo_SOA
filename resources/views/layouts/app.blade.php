d21174b713b286fd15574a5957c1085e38b724e7
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-25 10:18:11 -0500

    Incorporacion de JQuery-UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('jquery-ui-1.13.0.custom/jquery-ui.min.css') }}">
 
     <!--Icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
@@ -147,6 +148,9 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
+<script src="{{ asset('jquery-ui-1.13.0.custom/jquery-ui.min.js') }}"></script>
+
 <script src="https://maps.googleapis.com/maps/api/js?key=&callback=iniciarMapa"></script>
+
 
 </html>
